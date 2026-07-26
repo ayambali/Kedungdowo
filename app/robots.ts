ad3a2e648daba326d717b1b6d8d258b5fa996e03
Duta <dutaadips@gmail.com>
2026-07-26 19:11:44 +0700

    fixing seo

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "desakedungdowo.com";
 
   return {
     rules: {
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/admin", "/api"],
     },
-    sitemap: `${siteUrl}/sitemap.xml`,
+    sitemap: `https://desakedungdowo.com/sitemap.xml`,
   };
 }
